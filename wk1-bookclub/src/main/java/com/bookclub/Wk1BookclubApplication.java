e0eca92456d00c9d550c5bbe24c256a686ef7f0f
Richard Krasso <rkrasso@bellevue.edu>
2021-07-12 10:25:41 -0500

    initial commit.

---
+++ wk1-bookclub/src/main/java/com/bookclub/Wk1BookclubApplication.java
@@ -0,0 +1,13 @@
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk1BookclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wk1BookclubApplication.class, args);
+	}
+
+}
